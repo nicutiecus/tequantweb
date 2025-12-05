6d4e94386461123139b13fd2da80a85ff3732563
nicutiecus <100819560+nicutiecus@users.noreply.github.com>
2025-12-05 17:44:35 +0100

    Update settings.py

--- lmsapi/lmsapi/settings.py
+++ lmsapi/lmsapi/settings.py
@@ -136,10 +136,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field

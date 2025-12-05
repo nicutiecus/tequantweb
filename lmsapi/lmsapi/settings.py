0e4363902849773189da6209c647d76560ee67fb
nicutiecus <100819560+nicutiecus@users.noreply.github.com>
2025-12-05 17:02:27 +0100

    Updated Setting.py for production

--- lmsapi/lmsapi/settings.py
+++ lmsapi/lmsapi/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-61z8q)&lq68e7mkwpnh1*^badhd(e1z2&r9yux^8#f*l#g#)&d'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG', default=False, cast=bool) 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
 
 
 # Application definition

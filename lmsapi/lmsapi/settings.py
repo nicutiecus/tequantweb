30cd78260093917f543a777a0f673572ca72b0b2
nicutiecus <100819560+nicutiecus@users.noreply.github.com>
2025-12-06 08:03:21 +0100

    Set default SECRET_KEY for development builds

--- lmsapi/lmsapi/settings.py
+++ lmsapi/lmsapi/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-dummy-key-for-build-12345')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool) 

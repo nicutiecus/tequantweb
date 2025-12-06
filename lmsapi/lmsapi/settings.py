70dbd3f27d73d9fe0bee9d5f36c8fd6ba216615b
nicutiecus <100819560+nicutiecus@users.noreply.github.com>
2025-12-06 07:56:08 +0100

    Add 'lmsapi' to installed apps

--- lmsapi/lmsapi/settings.py
+++ lmsapi/lmsapi/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
+    'lmsapi',
     'main',
     'blog',
     'tequantweb',

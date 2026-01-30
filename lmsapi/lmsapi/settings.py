8c1012d51d94eb166e6a05e7cb3641ff2421abc9
Nikson Kejeh <niksonkejeh@gmail.com>
2026-01-30 09:58:36 +0100

    added CSRF trusted domains

--- lmsapi/lmsapi/settings.py
+++ lmsapi/lmsapi/settings.py
@@ -196,3 +196,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 #CORS_ALLOWED_ORIGINS =["http://localhost:3000",]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://api.tequant.ng",
+    "https://tequant.ng",       
+    "http://127.0.0.1:8000",    # Keep this for local testing
+]
